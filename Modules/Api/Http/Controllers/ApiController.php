a05775fd6222322a969a9581b8458dd40db4e676
mozhenzhu <251439200@qq.com>
2019-06-01 23:44:59 +0800

    hook

--- Modules/Api/Http/Controllers/ApiController.php
+++ Modules/Api/Http/Controllers/ApiController.php
@@ -8,6 +8,68 @@ use Illuminate\Routing\Controller;
 
 class ApiController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     * @return Response
+     */
+    public function index()
+    {
+        return view('api::index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     * @return Response
+     */
+    public function create()
+    {
+        return view('api::create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     * @param  Request $request
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+    }
+
+    /**
+     * Show the specified resource.
+     * @return Response
+     */
+    public function show()
+    {
+        return view('api::show');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     * @return Response
+     */
+    public function edit()
+    {
+        return view('api::edit');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     * @param  Request $request
+     * @return Response
+     */
+    public function update(Request $request)
+    {
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     * @return Response
+     */
+    public function destroy()
+    {
+    }
+
     public function hook(){
         dd('hook');
         $secret = "q20031120";
@@ -23,5 +85,4 @@ class ApiController extends Controller
         }
         http_response_code(404);
     }
-
 }
--- Modules/Api/Http/routes.php
+++ Modules/Api/Http/routes.php
@@ -1,7 +1,11 @@
 <?php
 
-Route::group(['middleware' => 'web', 'prefix' => 'api', 'namespace' => 'Modules\Api\Http\Controllers'], function()
-{
+Route::group(
+    ['middleware' => 'web', 'prefix' => 'api', 'namespace' => 'Modules\Api\Http\Controllers'],
+    function () {
+        Route::get('/', 'ApiController@index');
 
-    Route::get('/api/hook','ApiController@hook')->name('hook');
-});
+    }
+);
+Route::get('/api/hook', 'ApiController@hook');
+
