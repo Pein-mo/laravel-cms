75a7ad264f25b1d0d50a8e965998cd83a3430360
mozhenzhu <251439200@qq.com>
2019-06-01 23:49:20 +0800

    测试hook

--- Modules/Api/Http/routes.php
+++ Modules/Api/Http/routes.php
@@ -4,8 +4,9 @@ Route::group(
     ['middleware' => 'web', 'prefix' => 'api', 'namespace' => 'Modules\Api\Http\Controllers'],
     function () {
         Route::get('/', 'ApiController@index');
+        Route::get('/hook', 'ApiController@hook');
 
     }
 );
-Route::get('/api/hook', 'ApiController@hook');
+
 
