5e18024f8d49f623943f2527c8f470292ee14406
mozhenzhu <251439200@qq.com>
2019-06-01 23:06:32 +0800

    hook

--- Modules/Api/Http/Controllers/ApiController.php
+++ Modules/Api/Http/Controllers/ApiController.php
@@ -8,65 +8,20 @@ use Illuminate\Routing\Controller;
 
 class ApiController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     * @return Response
-     */
-    public function index()
-    {
-        return view('api::index');
+    public function hook(){
+        dd('hook');
+        $secret = "q20031120";
+        $path = "/www/wwwroot/lh.mo1120.com";
+        // Headers deliveried from GitHub
+        $signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
+        if ($signature) {
+            $hash = "sha1=".hash_hmac('sha1', file_get_contents("php://input"), $secret);
+            if (strcmp($signature, $hash) == 0) {
+                echo shell_exec("cd {$path} && /usr/bin/git reset --hard origin/master && /usr/bin/git clean -f && /usr/bin/git pull 2>&1");
+                exit();
+            }
+        }
+        http_response_code(404);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     * @return Response
-     */
-    public function create()
-    {
-        return view('api::create');
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     * @param  Request $request
-     * @return Response
-     */
-    public function store(Request $request)
-    {
-    }
-
-    /**
-     * Show the specified resource.
-     * @return Response
-     */
-    public function show()
-    {
-        return view('api::show');
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     * @return Response
-     */
-    public function edit()
-    {
-        return view('api::edit');
-    }
-
-    /**
-     * Update the specified resource in storage.
-     * @param  Request $request
-     * @return Response
-     */
-    public function update(Request $request)
-    {
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     * @return Response
-     */
-    public function destroy()
-    {
-    }
 }
--- Modules/Api/Http/routes.php
+++ Modules/Api/Http/routes.php
@@ -2,5 +2,6 @@
 
 Route::group(['middleware' => 'web', 'prefix' => 'api', 'namespace' => 'Modules\Api\Http\Controllers'], function()
 {
-    Route::get('/', 'ApiController@index');
+
+    Route::get('hook','ApiController@hook')->name('hook');
 });
