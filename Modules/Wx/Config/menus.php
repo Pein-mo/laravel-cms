c60492182a13498672b6de43245a35fee26c539f
mozhenzhu <251439200@qq.com>
2019-06-03 00:57:51 +0800

    微信

--- Modules/Wx/Config/menus.php
+++ Modules/Wx/Config/menus.php
@@ -1,29 +1,15 @@
 <?php return array (
-  'Wx' => 
+
+  'wx' =>
   array (
-    'title' => '系统管理',
+    'title' => '微信管理',
     'icon' => 'fa fa-navicon',
     'permission' => '权限标识',
-    'menus' => 
+    'menus' =>
     array (
-      0 => 
+      0 =>
       array (
-        'title' => '网站配置',
-        'permission' => '权限标识',
-        'url' => '链接地址',
-      ),
-    ),
-  ),
-  'wx' => 
-  array (
-    'title' => '微信基本配置管理',
-    'icon' => 'fa fa-navicon',
-    'permission' => '权限标识',
-    'menus' => 
-    array (
-      0 => 
-      array (
-        'title' => '微信基本配置管理',
+        'title' => '配置管理',
         'permission' => '',
         'url' => '/wx/wx_config',
       ),
