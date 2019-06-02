776efbee7b4b5491bf68b3c0c7d7ccecd3c22d99
mozhenzhu <251439200@qq.com>
2019-06-03 00:54:25 +0800

    微信

--- Modules/Wx/Http/routes.php
+++ Modules/Wx/Http/routes.php
@@ -1,13 +1,13 @@
 <?php
 
-Route::group(['middleware' => 'web', 'prefix' => 'wx', 'namespace' => 'Modules\Wx\Http\Controllers'], function()
+Route::group(['middleware' => ['web','auth:admin'], 'prefix' => 'wx', 'namespace' => 'Modules\Wx\Http\Controllers'], function()
 {
     Route::get('/', 'WxController@index');
 });
 
- 
+
 //wx_config-route
-Route::group(['middleware' => ['web'],'prefix'=>'wx','namespace'=>"Modules\Wx\Http\\Controllers"], 
+Route::group(['middleware' => ['web','auth:admin'],'prefix'=>'wx','namespace'=>"Modules\Wx\Http\\Controllers"],
 function () {
     Route::resource('wx_config', 'WxConfigController');
 });
