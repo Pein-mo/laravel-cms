e50c9bfb875a9c79f1848944caae0bdf07d32fbc
mozhenzhu <251439200@qq.com>
2019-06-04 01:14:33 +0800

    bug

--- public/addons/Article/Entities/Category.php
+++ public/addons/Article/Entities/Category.php
@@ -2,7 +2,8 @@
 
 namespace Modules\Article\Entities;
 
-use houdunwang\arr\Arr;
+
+use Houdunwang\Arr\Arr;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
@@ -20,10 +21,11 @@ class Category extends Model
         if (!is_null($category)) {
             foreach ($data as $k => $d) {
                 $data[$k]['_selected'] = $category['pid'] == $d['id'];
-                $data[$k]['_disabled'] = $category['id'] == $d['id'] || Arr::isChild($data, $d['id'], $category['id'], 'id');
+                $data[$k]['_disabled'] = $category['id'] == $d['id'] || (new Arr())->isChild($data,$d['id'],$category['id'],'id',$fidldPid='pid');
             }
         }
-        $data = (new Arr())->tree($data, 'name', 'id');
+        $data =(new Arr())->tree($data, 'name', 'id');;
+//        $data = (new Arr())->tree($data, 'name', 'id');
         return $data;
     }
 
