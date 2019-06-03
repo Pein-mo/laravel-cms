2a5ba94b18ebcf0ae2382b2e2782b1d042a86cc9
mozhenzhu <251439200@qq.com>
2019-06-04 01:19:54 +0800

    1

--- public/addons/Article/Http/routes.php
+++ public/addons/Article/Http/routes.php
@@ -18,15 +18,15 @@ Route::group(['middleware' => ['web', 'auth:admin'], 'prefix' => 'article', 'nam
         Route::get('template', 'TemplateController@index');
         Route::get('template/set/{name}', 'TemplateController@setDefaultTemplate');
     });
- 
+
 //slide-route
-Route::group(['middleware' => ['web', 'auth:admin'],'prefix'=>'article','namespace'=>"Modules\Article\Http\Controllers"], 
+Route::group(['middleware' => ['web', 'auth:admin'],'prefix'=>'article','namespace'=>"Modules\Article\Http\Controllers"],
 function () {
     Route::resource('slide', 'SlideController')->middleware("permission:admin,resource");
 });
- 
+
 //comment-route
-Route::group(['middleware' => ['web', 'auth:web'],'prefix'=>'article','namespace'=>"Modules\Article\Http\Controllers"],
+Route::group(['middleware' => ['web', 'auth:admin'],'prefix'=>'article','namespace'=>"Modules\Article\Http\Controllers"],
 function () {
     Route::resource('comment', 'CommentController');
 });
