9fe3ae9fd23d9772c7c625123acd63fc05b546cf
mozhenzhu <251439200@qq.com>
2019-06-03 17:06:59 +0800

    微信配置完成

--- Modules/Wx/Http/Controllers/WxConfigController.php
+++ Modules/Wx/Http/Controllers/WxConfigController.php
@@ -1,30 +1,31 @@
 <?php
+
 namespace Modules\Wx\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Modules\Wx\Entities\WxConfig;
 use Modules\Wx\Http\Requests\WxConfigRequest;
+
 class WxConfigController extends Controller
 {
     //显示列表
-    public function index()
+    public function index(WxConfig $wxConfig)
     {
-        $data = WxConfig::paginate(10);
-        return view('wx::wx_config.index', compact('data'));
+        $wx_config = $wxConfig->pluck('value', 'name');
+        return view('wx::wx_config.index', compact('wx_config'));
     }
 
 
-
     //保存数据
-    public function store(WxConfigRequest $request,WxConfig $wx_config)
+    public function store(WxConfigRequest $request, WxConfig $wx_config)
     {
-//        dd(111);
         $wx_config->truncate();
-        foreach ($request->all() as $name =>$value){
-            $wx_config->insert(['name'=>$name,'value'=>$value]);
+        foreach ($request->all() as $name => $value) {
+            if ($name != '_token') {
+                $wx_config->insert(['name' => $name, 'value' => $value]);
+            }
         }
-
         return redirect('/wx/wx_config')->with('success', '保存成功');
     }
 
