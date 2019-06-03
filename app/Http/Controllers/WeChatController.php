b9a7c9c7a40773c06e1d64165d9f60256e23efbe
mozhenzhu <251439200@qq.com>
2019-06-03 17:30:47 +0800

    微信接入

---
+++ app/Http/Controllers/WeChatController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Houdunwang\WeChat\WeChat;
+use Illuminate\Http\Request;
+use Modules\Wx\Entities\WxConfig;
+
+class WeChatController extends Controller
+{
+    public function handler(WxConfig $wxConfig){
+        $config =array_merge(include base_path('config').'/wechat.php',$wxConfig->pluck('value','name'));
+
+        WeChat::config($config)->valid();
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -29,10 +29,15 @@ Route::group(
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-
+//上传图片
 Route::any('/upload', 'UploadController@make');
 Route::any('/upload-simditor', 'UploadController@simditor');
 
+//邮箱验证
 Route::get('/check_mail_show','UserController@checkMailShow')->name('check_mail_show');
 Route::get('/send_mail_token','UserController@sendMailToken')->name('send_mail_token');
 Route::get('/check_register_mail/{token}','UserController@checkUserMail')->name('check_register_mail');
+
+
+//微信绑定
+Route::any('wechat','WeChatController@handler');
