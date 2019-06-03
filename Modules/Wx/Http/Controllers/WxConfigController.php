7a79e16aaf9e5acd3e96e536dcfcd8f35496502b
mozhenzhu <251439200@qq.com>
2019-06-03 16:43:32 +0800

    微信配置修改

--- Modules/Wx/Http/Controllers/WxConfigController.php
+++ Modules/Wx/Http/Controllers/WxConfigController.php
@@ -14,44 +14,18 @@ class WxConfigController extends Controller
         return view('wx::wx_config.index', compact('data'));
     }
 
-    //创建视图
-    public function create(WxConfig $wx_config)
-    {
-        return view('wx::wx_config.create',compact('wx_config'));
-    }
+
 
     //保存数据
     public function store(WxConfigRequest $request,WxConfig $wx_config)
     {
-        $wx_config->fill($request->all());
-        $wx_config->save();
+        $wx_config->truncate();
+        foreach ($request->all() as $name =>$value){
+            $wx_config->insert(['name'=>$name,'value'=>$value]);
+        }
 
         return redirect('/wx/wx_config')->with('success', '保存成功');
     }
 
-    //显示记录
-    public function show(WxConfig $field)
-    {
-        return view('wx::wx_config.show', compact('field'));
-    }
 
-    //编辑视图
-    public function edit(WxConfig $wx_config)
-    {
-        return view('wx::wx_config.edit', compact('wx_config'));
-    }
-
-    //更新数据
-    public function update(WxConfigRequest $request, WxConfig $wx_config)
-    {
-        $wx_config->update($request->all());
-        return redirect('/wx/wx_config')->with('success','更新成功');
-    }
-
-    //删除模型
-    public function destroy(WxConfig $wx_config)
-    {
-        $wx_config->delete();
-        return redirect('wx/wx_config')->with('success','删除成功');
-    }
 }
