ada08112b6478488281b3be8760ebd9105eb61ee
mozhenzhu <251439200@qq.com>
2019-06-04 11:10:48 +0800

    222

--- public/addons/Article/Services/TemplateService.php
+++ public/addons/Article/Services/TemplateService.php
@@ -12,7 +12,7 @@ class TemplateService
 {
     public function all()
     {
-        dd(glob(public_path('uploads/*')));
+        dd(glob(public_path('templates/*')));
         $dirs = glob(public_path('templates/*'));
 
         $configs = [];
