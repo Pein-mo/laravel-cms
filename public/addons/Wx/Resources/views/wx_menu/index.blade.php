ae4efd83733475ef11477f2d708414eacf3dd025
mozhenzhu <251439200@qq.com>
2019-06-05 20:56:53 +0800

    wx_menu

--- public/addons/Wx/Resources/views/wx_menu/index.blade.php
+++ public/addons/Wx/Resources/views/wx_menu/index.blade.php
@@ -38,70 +38,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="col-sm-8">
-                        <div class="card">
-                            <div class="card-header">
-                                基本设置
-                            </div>
-                            <div class="card-block">
-                                <div class="form-group row">
-                                    <label for="inputSmall"
-                                           class="col-12 col-sm-3 col-form-label text-sm-right">菜单名称</label>
-                                    <div class="col-12 col-sm-8 col-lg-6">
-                                        <input type="text" class="form-control form-control-sm" name="name">
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="card">
-                            <div class="card-header">
-                                菜单
-                            </div>
-                            <div class="card-block">
-                                <div class="form-group row">
-                                    <label for="inputSmall"
-                                           class="col-12 col-sm-3 col-form-label text-sm-right">菜单</label>
-                                    <div class="col-12 col-sm-8 col-lg-6">
-                                        <input type="text" class="form-control form-control-sm" v-model="active.name">
-                                    </div>
-                                </div>
-                                <div class="form-group row pt-1 pb-1">
-                                    <label class="col-12 col-sm-3 col-form-label text-sm-right">动作</label>
-                                    <div class="col-12 col-sm-8 col-lg-6 form-check mt-2">
-                                        <label class="custom-control custom-radio custom-control-inline">
-                                            <input type="radio" name="radio-inline" v-model="active.type" value="view"
-                                                   class="custom-control-input">
-                                            <span class="custom-control-label">链接</span>
-                                        </label>
-                                        <label class="custom-control custom-radio custom-control-inline">
-                                            <input type="radio" name="radio-inline" v-model="active.type" value="click"
-                                                   class="custom-control-input">
-                                            <span class="custom-control-label">关键词</span>
-                                        </label>
-                                    </div>
-                                </div>
-                                <div class="form-group row" v-if="active.type=='view'">
-                                    <label for="inputSmall"
-                                           class="col-12 col-sm-3 col-form-label text-sm-right">链接</label>
-                                    <div class="col-12 col-sm-8 col-lg-6">
-                                        <input type="text" class="form-control form-control-sm" v-model="active.url">
-                                    </div>
-                                </div>
-                                <div class="form-group row" v-if="active.type=='click'">
-                                    <label for="inputSmall"
-                                           class="col-12 col-sm-3 col-form-label text-sm-right">关键词</label>
-                                    <div class="col-12 col-sm-8 col-lg-6">
-                                        <input type="text" class="form-control form-control-sm" v-model="active.key">
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="card-footer text-muted">
-                                <textarea name="data" hidden>@{{ menus }}</textarea>
-                                <button class="btn-primary btn">保存菜单</button>
-                            </div>
-                        </div>
-                    </div>
+                    
                 </div>
 
             </div>
