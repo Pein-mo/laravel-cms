e32ff76bf02a6dd14874424e0dbd3f3c72525753
mozhenzhu <251439200@qq.com>
2019-06-05 20:35:31 +0800

    wx_menu

--- public/addons/Wx/Resources/views/menu/index.js
+++ public/addons/Wx/Resources/views/menu/index.js
@@ -1,5 +1,10 @@
 define(['vue'],function (vue) {
     return function () {
-        alert(3);
+        new vue({
+            el:'app',
+            data:{
+                menus:333
+            }
+        });
     }
 })
