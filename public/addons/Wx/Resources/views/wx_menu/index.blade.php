9be40f917eadd2c2249256dd1c8b962b5bf63699
mozhenzhu <251439200@qq.com>
2019-06-06 11:07:18 +0800

    wx_menu

--- public/addons/Wx/Resources/views/wx_menu/index.blade.php
+++ public/addons/Wx/Resources/views/wx_menu/index.blade.php
@@ -1,116 +1,57 @@
 @extends('admin::layouts.master')
-@section('head')
-    <link rel="stylesheet" href="{{asset('addons/Wx/Resources/views/menu/index.css')}}">
-@endsection
 @section('content')
     <div class="card" id="app">
         <div class="card-header">微信菜单管理</div>
-
-        <form action="/wx/wx_menu" method="post">
-            <div class="card-body card-body-contrast">
-                @csrf
-                <div class="row">
-                    <div class="col-sm-4">
-                        <div class="mobile">
-                            <div class="menu-container">
-                                <div class="menu" v-for="(v,i) in menus">
-                                    <h5 >
-                                        <i @click="delMenu(i)" class="fa fa-minus-square" aria-hidden="true"></i>
-                                        @{{v.name}}
-                                    </h5>
-                                    <dl>
-                                        <dd v-if="v.sub_button.length<5"><i @click="addSubMenu(v)" class="fa fa-plus-square" aria-hidden="true"></i>添加子菜单</dd>
-{{--                                        <dd v-if="(m,n) in v.sub_button"><i class="fa fa-minus-square" aria-hidden="true"></i></dd>--}}
-                                        <dd v-for="(m,n) in v.sub_button">
-                                            <i @click="delSubMenu(v,n)" class="fa fa-minus-square" aria-hidden="true"></i>  @{{ m.name }}
-                                        </dd>
-                                    </dl>
-                                </div>
-                                <div class="menu" v-if="menus.length<3">
-                                    <h5 @click="add()">添加</h5>
-                                </div>
-
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-8">
-                        <div class="card">
-                            <div class="card-header">
-                                基本设置
-                            </div>
-                            <div class="card-block">
-                                <div class="form-group row">
-                                    <label for="inputSmall"
-                                           class="col-12 col-sm-3 col-form-label text-sm-right">菜单名称</label>
-                                    <div class="col-12 col-sm-8 col-lg-6">
-                                        <input type="text" class="form-control form-control-sm" name="name">
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="card">
-                            <div class="card-header">
-                                菜单
-                            </div>
-                            <div class="card-block">
-                                <div class="form-group row">
-                                    <label for="inputSmall"
-                                           class="col-12 col-sm-3 col-form-label text-sm-right">菜单</label>
-                                    <div class="col-12 col-sm-8 col-lg-6">
-                                        <input type="text" class="form-control form-control-sm" v-model="active.name">
-                                    </div>
-                                </div>
-                                <div class="form-group row pt-1 pb-1">
-                                    <label class="col-12 col-sm-3 col-form-label text-sm-right">动作</label>
-                                    <div class="col-12 col-sm-8 col-lg-6 form-check mt-2">
-                                        <label class="custom-control custom-radio custom-control-inline" >
-                                            <input type="radio" name="radio-inline" v-model="active.type" value="view"
-                                                   class="custom-control-input">
-                                            <span class="custom-control-label">链接</span>
-                                        </label>
-                                        <label class="custom-control custom-radio custom-control-inline" >
-                                            <input type="radio" name="radio-inline" v-model="active.type" value="click"
-                                                   class="custom-control-input">
-                                            <span class="custom-control-label">关键词</span>
-                                        </label>
-                                    </div>
-                                </div>
-                                <div class="form-group row" v-if="active.type =='view'">
-                                    <label for="inputSmall"
-                                           class="col-12 col-sm-3 col-form-label text-sm-right">链接</label>
-                                    <div class="col-12 col-sm-8 col-lg-6">
-                                        <input type="text" class="form-control form-control-sm" v-model="active.url">
-                                    </div>
-                                </div>
-                                <div class="form-group row" v-if="active.type =='click'">
-                                    <label for="inputSmall"
-                                           class="col-12 col-sm-3 col-form-label text-sm-right">关键词</label>
-                                    <div class="col-12 col-sm-8 col-lg-6">
-                                        <input type="text" class="form-control form-control-sm" v-model="active.key">
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="card-footer text-muted">
-                                <textarea name="data" hidden>@{{ menus }}</textarea>
-                                <button class="btn-primary btn">保存菜单</button>
-                            </div>
-                        </div>
-                    </div>
+        <div class="tab-container">
+            <ul role="tablist" class="nav nav-tabs">
+                <li class="nav-item"><a href="/wx/wx_menu" class="nav-link active">微信菜单列表</a></li>
+                <li class="nav-item"><a href="/wx/wx_menu/create" class="nav-link">添加微信菜单</a></li>
+            </ul>
+            <div class="card card-contrast card-border-color-success">
+                <div class="card-body">
+                    <table class="table">
+                        <thead>
+                        <tr>
+                            <th style="width: 10%;">编号</th>
+                            <th>菜单名称</th>
+                            <th>创建时间</th>
+                            <th>修改时间</th>
+                            <th>&nbsp;</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($data as $d)
+                            <tr>
+                                <td>{!! $d['id'] !!}</td>
+                                <td>{!! $d['name'] !!}</td>
+                                <td>{!! $d['created_at'] !!}</td>
+                                <td>{!! $d['updated_at'] !!}</td>
+                                <td class="text-right">
+                                    <a href="/wx/wx_menu/push/{{$d['id']}}" class="btn btn-secondary">推送到微信端</a>
+                                    <a href="/wx/wx_menu/{{$d['id']}}/edit" class="btn btn-secondary">编辑</a>
+                                    <button type="button" class="btn btn-secondary btn-danger" onclick="del({{$d['id']}},this)">删除</button>
+                                    <form action="/wx/wx_menu/{{$d['id']}}" hidden method="post">
+                                        @csrf @method('DELETE')
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
                 </div>
-
             </div>
-            <div class="card-footer text-muted">
-                <button class="btn btn-primary offset-sm-2">保存提交</button>
-            </div>
-        </form>
+        </div>
+    </div>
+    <div class="float-right">
+        {!!  $data->links() !!}
     </div>
 @endsection
 @section('scripts')
     <script>
-        require(["{{asset('addons/Wx/Resources/views/menu/index.js')}}"], function (index) {
-            index([]);
-        })
+        function del(id, el) {
+            if (confirm('确定删除吗？')) {
+                $(el).next('form').trigger('submit')
+            }
+        }
     </script>
-
 @endsection
