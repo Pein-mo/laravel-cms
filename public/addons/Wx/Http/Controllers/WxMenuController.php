502b9bb19d63dbc158f88cd1c4c37738746c08e1
mozhenzhu <251439200@qq.com>
2019-06-06 21:41:35 +0800

    wx_menu22

--- public/addons/Wx/Http/Controllers/WxMenuController.php
+++ public/addons/Wx/Http/Controllers/WxMenuController.php
@@ -63,11 +63,13 @@ class WxMenuController extends Controller
     public function push(WxMenu $menu,WeChatService $chatService){
 
         $data = $chatService->instance('button')->create(['button'=>$menu['data']]);
-        dd($data);
-        if($data['errcode'==0]){
+
+        if($data['errcode']==0){
             WxMenu::where('id',$menu['id'])->save(['sta'=>1]);
             WxMenu::whereNotIn('id',$menu['id'])->save(['sta'=>0]);
             return back()->with('success','微信菜单推送成功，请稍后在微信查看');
+        }else{
+            return back()->with('danger',$data['errmsg']);
         }
     }
 }
