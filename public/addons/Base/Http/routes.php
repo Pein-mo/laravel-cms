a228a2252a142bec5a6e8d5e41abd5b6d09bd364
mozhenzhu <251439200@qq.com>
2019-06-08 19:18:03 +0800

    wx_rule

--- public/addons/Base/Http/routes.php
+++ public/addons/Base/Http/routes.php
@@ -1,13 +1,10 @@
 <?php
 
-Route::group(['middleware' => 'web', 'prefix' => 'base', 'namespace' => 'Modules\Base\Http\Controllers'], function()
-{
-    Route::get('/', 'BaseController@index');
-});
+
 
 
 //base-route
 Route::group(['middleware' => ['web','auth:admin'],'prefix'=>'base','namespace'=>"Modules\base\Http\\Controllers"],
 function () {
-    Route::resource('base', 'baseController');
+    Route::resource('base', 'BaseController');
 });
