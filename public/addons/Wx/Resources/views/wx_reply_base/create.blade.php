e87c86730638bb9523fc9e11a4e4a8bc7186d7ea
mozhenzhu <251439200@qq.com>
2019-06-08 12:11:51 +0800

    wx_rule

--- public/addons/Wx/Resources/views/wx_reply_base/create.blade.php
+++ public/addons/Wx/Resources/views/wx_reply_base/create.blade.php
@@ -26,20 +26,26 @@
     </div>
 @endsection
 <script>
-    require(['vue','hdjs'],function (vue,hdjs) {
+    // require(['vue','hdjs'],function (vue,hdjs) {
+    //     new vue({
+    //         el:"#app",
+    //         data:{
+    //             contents:[{content:''}]
+    //         },
+    //         methods:{
+    //             add(){
+    //                 this.contents.push({content:''});
+    //             },
+    //             del(pos){
+    //                 this.contents.splice(pos,1)
+    //             }
+    //         }
+    //     })
+    // });
+    require(['vue'],function (vue) {
         new vue({
             el:"#app",
-            data:{
-                contents:[{content:''}]
-            },
-            methods:{
-                add(){
-                    this.contents.push({content:''});
-                },
-                del(pos){
-                    this.contents.splice(pos,1)
-                }
-            }
+            data:{}
         })
-    });
+    })
 </script>
