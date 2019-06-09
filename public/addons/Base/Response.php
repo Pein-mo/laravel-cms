53c22288bab2190e58284935e7cbf527f2e9638a
mozhenzhu <251439200@qq.com>
2019-06-09 21:12:56 +0800

    wx_rule

--- public/addons/Base/Response.php
+++ public/addons/Base/Response.php
@@ -10,6 +10,6 @@ class Response
 {
     public function handle($rule){
         $instance = new Message();
-        return $instance->text('你好,我在这里');
+        return $instance->text('你好,我在这里'.$rule['id']);
     }
 }
