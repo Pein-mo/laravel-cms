b0a803ce3498e6e75c38e14e5bf9abb2ad85747a
mozhenzhu <251439200@qq.com>
2019-06-09 21:28:55 +0800

    wx_rule

--- public/addons/Base/Response.php
+++ public/addons/Base/Response.php
@@ -12,7 +12,7 @@ class Response
     public function handle($rule){
         $instance = new Message();
         $base = Base::firstOrNew(['rule_id'=>$rule['id']]);
-        $contents = json_decode($base,true);
+        $contents = json_decode($base->content,true);
         return $instance->text(array_random($contents)['content']);
     }
 }
