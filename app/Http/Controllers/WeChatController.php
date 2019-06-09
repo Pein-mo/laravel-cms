5cd3e5a09d4ddb394e1cdc81a967423e6a8dbd71
mozhenzhu <251439200@qq.com>
2019-06-09 19:44:05 +0800

    wx_rule

--- app/Http/Controllers/WeChatController.php
+++ app/Http/Controllers/WeChatController.php
@@ -2,16 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use Houdunwang\WeChat\Build\Message\Message;
 use Houdunwang\WeChat\WeChat;
 use Illuminate\Http\Request;
 use Modules\Wx\Entities\WxConfig;
 
 class WeChatController extends Controller
 {
-    public function handler(WxConfig $wxConfig){
+
+    public function __construct(WxConfig $wxConfig)
+    {
         $config =array_merge(include base_path('config').'/wechat.php',$wxConfig->pluck('value','name')->toArray());
         (new WeChat)->config($config);
         (new WeChat)->valid();
+    }
+
+    public function handler(){
+        //消息管理模块
+        $instance = new Message;
+        //判断是否是文本消息
+        if ($instance->isTextMsg())
+        {
+            //向用户回复消息
+            return $instance->text('后盾人收到你的消息了...:' . $instance->Content);
+        }
 
     }
 }
