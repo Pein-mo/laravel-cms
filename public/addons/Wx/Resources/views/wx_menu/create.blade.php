292ea31cbd959c2dc142a2e844ca4324568b6588
mozhenzhu <251439200@qq.com>
2019-06-10 18:22:49 +0800

    wx_rule

--- public/addons/Wx/Resources/views/wx_menu/create.blade.php
+++ public/addons/Wx/Resources/views/wx_menu/create.blade.php
@@ -96,8 +96,8 @@
         {{--require(["{{asset('addons/Wx/Resources/views/menu/index.js')}}"], function (index) {--}}
         {{--    index([]);--}}
         {{--});--}}
-        require(['{{asset('addons/News/Resources/views/news/js/news.js')}}'], function (news) {
-            news([{title: '后盾人', 'discription': 'houdunren.com', 'picurl': '/plugin/hdjs/image/nopic.jpg', 'url': 'http://www.houdunwang.com'}])
+        require(['{{asset('addons/News/Resources/views/news/js/news.js')}}'], function (news1) {
+            news1([{title: '后盾人', 'discription': 'houdunren.com', 'picurl': '/plugin/hdjs/image/nopic.jpg', 'url': 'http://www.houdunwang.com'}])
         })
     </script>
 @endsection
