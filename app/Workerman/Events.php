fc49f17995e2d98da20868be4f3e1f5b72155ac2
mozhenzhu <251439200@qq.com>
2019-06-18 18:15:39 +0800

    api contents

--- app/Workerman/Events.php
+++ app/Workerman/Events.php
@@ -4,6 +4,8 @@
 namespace App\Workerman;
 
 
+use GatewayWorker\Lib\Gateway;
+
 class Events
 {
     public static function onWorkerStart($businessWorker)
@@ -12,6 +14,12 @@ class Events
 
     public static function onConnect($client_id)
     {
+        global $num;
+        // 向当前client_id发送数据
+        Gateway::sendToClient($client_id, "Hello $client_id\r\n");
+        // 向所有人发送
+        Gateway::sendToAll("$client_id login\r\n");
+        echo 'connect'.++$num.":".$client_id."\n";
     }
 
     public static function onWebSocketConnect($client_id, $data)
@@ -20,6 +28,8 @@ class Events
 
     public static function onMessage($client_id, $message)
     {
+        // 向所有人发送
+        Gateway::sendToAll("$client_id said $message\r\n");
     }
 
     public static function onClose($client_id)
